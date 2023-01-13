6435b5d9334397cf3d5a992fcbcc78d70f8b489c
FLAKE_MDM <flake333@yandex.ru>
2023-01-13 23:54:00 +0200

    edit tdr 3

--- TDR/js/main.js
+++ TDR/js/main.js
@@ -6,36 +6,39 @@ $(".btn-menu, .close-menu").click(function(e){
 
 
 //animation
-const animItems = document.querySelectorAll('.anim-items');
+if(window.innerWidth > 767){
+    const animItems = document.querySelectorAll('.anim-items');
 
-if (animItems.length > 0) {
-    window.addEventListener('scroll', animOnScroll);
-    function animOnScroll(params) {
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index];
-            const animItemHeight = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 4;
-
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
-
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                animItem.classList.add('_active');
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll(params) {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+    
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+    
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+    
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_active');
+                }
             }
         }
+        function offset(e) {
+            const rect = e.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + screenLeft }
+        }
+        animOnScroll();
     }
-    function offset(e) {
-        const rect = e.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return { top: rect.top + scrollTop, left: rect.left + screenLeft }
-    }
-    animOnScroll();
 }
+
 
 // validation
 let loginForm = document.querySelector("#login-form");
